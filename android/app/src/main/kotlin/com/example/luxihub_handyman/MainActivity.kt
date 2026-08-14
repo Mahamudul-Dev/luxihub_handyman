92a89b526c7e52f4a1a9395763edb845cae0e97f
Mahamudul.Dev <mahamudul.dev@gmail.com>
2026-08-14 22:03:12 +0600

    refactor: remove old MainActivity files and add error log for Kotlin daemon compilation failures

--- android/app/src/main/kotlin/com/example/luxihub_handyman/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.luxihub_handyman
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/luxihub/handyman_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.luxihub.handyman_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
